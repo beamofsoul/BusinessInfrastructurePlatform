ca137397f9114fc735f324a2a3c660a268a83ff3
beamofsoul <beamofsoul@sina.com>
2017-03-31 15:53:17 +0800

    Update ScheduleConfig.java

--- src/main/java/com/beamofsoul/springboot/management/schedule/ScheduleConfig.java
+++ src/main/java/com/beamofsoul/springboot/management/schedule/ScheduleConfig.java
@@ -11,6 +11,14 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class ScheduleConfig {
 
+// * 第一位，表示秒，取值0-59
+// * 第二位，表示分，取值0-59
+// * 第三位，表示小时，取值0-23
+// * 第四位，日期天/日，取值1-31
+// * 第五位，日期月份，取值1-12
+// * 第六位，星期，取值1-7，星期一，星期二...，注：不是第1周，第二周的意思
+//           另外：1表示星期天，2表示星期一。
+// * 第7为，年份，可以留空，取值1970-2099
 //	//每5秒执行一次
 //	@Scheduled(cron="0/5 * * * * ?")
 //	public void scheduler() {
